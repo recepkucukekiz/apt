e66a9e1e83e13c2bb34a9af8d2ef2455d51c73ba
tahatakgungor <tahatakgungor@gmail.com>
2022-03-25 00:02:00 +0300

    Guncel5

--- lib/pages/daireEkle.dart
+++ lib/pages/daireEkle.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class DaireEkle extends StatelessWidget {
   const DaireEkle({Key? key}) : super(key: key);
-
+//asdasd
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
